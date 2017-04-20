37b34d52f8c27b5835057bba348c916391f4d4b7
n8houl <n8houl@gmail.com>
2017-04-20 01:08:32 -0400

    Specular Lighting
    Added specular lighting, and configurable paramaters for each entity

--- src/engineTest/MainGameLoop.java
+++ src/engineTest/MainGameLoop.java
@@ -24,16 +24,19 @@ public class MainGameLoop {
 		Renderer renderer = new Renderer(shader);
 		
 		RawModel model = OBJLoader.loadObjModel("dragon", loader);
-		ModelTexture texture = new ModelTexture(loader.loadTexture("white"));
+		ModelTexture texture = new ModelTexture(loader.loadTexture("purple"));
 		TexturedModel texturedModel = new TexturedModel(model, texture);
-		Entity entity = new Entity(texturedModel, new Vector3f(0, 0, -25), 0, 0, 0, 1);
 		
-		Light light = new Light(new Vector3f(0, 0, -20), new Vector3f(1, 1, 1));
+		texture.setShineDamper(10);
+		texture.setReflectivity(1);
+		
+		Entity entity = new Entity(texturedModel, new Vector3f(0, -5, -25), 0, 0, 0, 1);	
+		Light light = new Light(new Vector3f(200, 200, 100), new Vector3f(1, 1, 1));
 		
 		Camera camera = new Camera();
 		
 		while(!Display.isCloseRequested()) {
-			entity.increaseRotation(0, 2, 0);
+			entity.increaseRotation(0, 0, 0);
 			camera.move();
 			renderer.prepare();
 			shader.start();
--- src/renderEngine/Renderer.java
+++ src/renderEngine/Renderer.java
@@ -1,9 +1,21 @@
 package renderEngine;
 
-import static org.lwjgl.opengl.GL11.*;
-import static org.lwjgl.opengl.GL13.*;
-import static org.lwjgl.opengl.GL20.*;
-import static org.lwjgl.opengl.GL30.*;
+import static org.lwjgl.opengl.GL11.GL_COLOR_BUFFER_BIT;
+import static org.lwjgl.opengl.GL11.GL_DEPTH_BUFFER_BIT;
+import static org.lwjgl.opengl.GL11.GL_DEPTH_TEST;
+import static org.lwjgl.opengl.GL11.GL_TEXTURE_2D;
+import static org.lwjgl.opengl.GL11.GL_TRIANGLES;
+import static org.lwjgl.opengl.GL11.GL_UNSIGNED_INT;
+import static org.lwjgl.opengl.GL11.glBindTexture;
+import static org.lwjgl.opengl.GL11.glClear;
+import static org.lwjgl.opengl.GL11.glClearColor;
+import static org.lwjgl.opengl.GL11.glDrawElements;
+import static org.lwjgl.opengl.GL11.glEnable;
+import static org.lwjgl.opengl.GL13.GL_TEXTURE0;
+import static org.lwjgl.opengl.GL13.glActiveTexture;
+import static org.lwjgl.opengl.GL20.glDisableVertexAttribArray;
+import static org.lwjgl.opengl.GL20.glEnableVertexAttribArray;
+import static org.lwjgl.opengl.GL30.glBindVertexArray;
 
 import org.lwjgl.opengl.Display;
 import org.lwjgl.util.vector.Matrix4f;
@@ -12,6 +24,7 @@ import entities.Entity;
 import models.RawModel;
 import models.TexturedModel;
 import shaders.StaticShader;
+import textures.ModelTexture;
 import tools.MathHelper;
 
 public class Renderer {
@@ -31,7 +44,7 @@ public class Renderer {
 	public void prepare() {
 		glEnable(GL_DEPTH_TEST);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-		glClearColor(0, 0, 0, 1);
+		glClearColor(0.2f, 0, 0, 1);
 	}
 	
 	public void render(Entity entity, StaticShader shader) {
@@ -46,7 +59,8 @@ public class Renderer {
 		Matrix4f transformationMatrix = MathHelper.createTransformationMatrix(entity.getPosition(), entity.getRotX(), entity.getRotY(), entity.getRotZ(), entity.getScale());
 		
 		shader.loadTransformationMatrix(transformationMatrix);
-		
+		ModelTexture texture = texturedModel.getTexture();
+		shader.loadShineVariables(texture.getShineDamper(), texture.getReflectivity());
 		
 		glActiveTexture(GL_TEXTURE0);
 		glBindTexture(GL_TEXTURE_2D, texturedModel.getTexture().getTextureID());
